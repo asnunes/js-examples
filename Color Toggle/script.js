2243c29efd29945365bd435a93e866b1b587785c
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2018-09-11 22:28:36 -0300

    Color Toggle

--- Color Toggle/script.js
+++ Color Toggle/script.js
@@ -1,12 +1,5 @@
 var button = document.querySelector("button");
-var body = document.querySelector("body");
-var blue = false;
 
 button.addEventListener("click", function(){
-	if(blue){
-		body.style.background = "white";
-	} else{
-		body.style.background = "#3498db";
-	}
-	blue = !blue;
+	document.body.classList.toggle("blue");
 });
