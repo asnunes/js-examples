c6669de679077166c81dc4dd0f63234aca0fbcdc
Alexandre Nunes <alexandrenunes@poli.ufrj.br>
2018-09-24 22:56:21 -0300

    Prototype

--- Prototype  Example/proto.js
+++ Prototype  Example/proto.js
@@ -7,12 +7,18 @@ function Vehicle(make, model, year){
 
     this.isRunning = false;
 
-    this.honk = function(){
-        if(this.isRunning){
-            console.log("beep!");
-        }
-    }
 }
 
-Vehicle.prototype.turnOn = function(){ this.isRunning = true};
-Vehicle.prototype.turnOff = function(){ this.isRunning = false};
+Vehicle.prototype.turnOn = function(){ 
+    this.isRunning = true
+};
+
+Vehicle.prototype.turnOff = function(){
+     this.isRunning = false
+    };
+
+Vehicle.prototype.honk = function(){
+    if(this.isRunning){
+        console.log("beep!");
+    }
+}
